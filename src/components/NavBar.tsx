6881cfaafd7a3324259878d1b43c7057cd125df4
izulhq <55051387+izulhq@users.noreply.github.com>
2026-05-03 20:10:39 +0700

    remove wishlist from navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 import { ChevronUp, Table2, X } from "lucide-react";
 import Dropdown from "./Dropdown";
 import TabsFloating from "./TabsFloating";
-import WishlistFloating from "./WishlistFloating";
-import { FaHeart, FaQuestion } from "react-icons/fa6";
+import { FaQuestion } from "react-icons/fa6";
 
 export default function NavBar({
   view,
@@ -108,19 +107,6 @@ export default function NavBar({
                 </button>
               </div>
 
-              {/* Wishlist button */}
-              <div className="relative">
-                <button
-                  onClick={toggleWishlist}
-                  className="p-2 md:p-2 rounded-full shadow-md bg-gradient-to-r from-red-600 to-red-500 hover:bg-gradient-to-l group"
-                >
-                  <FaHeart className="w-6 h-6 md:w-5 md:h-5 text-white" />
-                  <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 px-2 py-1 border border-gray-400 shadow-md bg-white text-gray-800 text-sm rounded-md opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
-                    Wishlist
-                  </div>
-                </button>
-              </div>
-
               {/* ChevronUp button */}
               <div className="relative">
                 <button
@@ -156,9 +142,6 @@ export default function NavBar({
             />
           </div>
         </div>
-      )}
-      {showWishlist && (
-        <WishlistFloating isVisible={true} toggleWishlist={toggleWishlist} />
       )}
     </>
   );
